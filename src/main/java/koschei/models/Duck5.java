aeeea4ce7608e0db68db7687efb625e496d3d88d
Ilia <dkmplis@mail.ru>
2025-08-22 13:07:33 +0300

    done!!

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,11 +1,13 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
     @Autowired
+    @Qualifier("egg6")
     private Egg6 egg;
     @Override
     public String toString() {
